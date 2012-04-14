ac6a701af5970ea886bcf4abbe09befa9cc836d3
jeff oconnell <jeff.oconnell@gmail.com>
2012-04-14 15:33:26 -0400

    added header to the header

--- memsic_accelerometer/tilt_buzzer/tilt_buzzer.h
+++ memsic_accelerometer/tilt_buzzer/tilt_buzzer.h
@@ -1,4 +1,20 @@
 
+//
+// tilt buzzer
+//
+// simple hack which shows and sounds an alarm when an accelerometer detects tilt
+//
+// author: @jefforulez
+//
+
+//
+//  ideas code and ideas stolen from:
+//
+// - http://www.instructables.com/id/Play-the-French-Can-Can-Using-an-Arduino-and-Buzze/
+// - http://www.arduino.cc/en/Tutorial/AccelerometerMemsic2125
+//
+
+
 #ifndef rulez_tilt_buzzer_h
 #define rulez_tilt_buzzer_h
 
