2d4487e704a58992d844ff94ace2593183c94064
jeff oconnell <jeff.oconnell@gmail.com>
2012-04-14 22:34:11 -0400

    updated header comment

--- lolshield/nygiants/nygiants.cpp
+++ lolshield/nygiants/nygiants.cpp
@@ -7,6 +7,11 @@
 // author: @jefforulez
 //
 
+//
+// based on lol shield library sample code
+// http://code.google.com/p/lolshield/
+//
+
 #include "nygiants.h"
 
 //
@@ -45,19 +50,15 @@ void loop()
 
 void DisplayBitMap()
 {
-	bool run = true ;     // While this is true, the screen updates
-	unsigned long data ;  // Temporary storage of the row data
+	unsigned long data ;
 	
 	for ( uint8_t line = 0 ; line < 9 ; line++ ) 
 	{
-
-		// Here we fetch data from program memory with a pointer.
-		//
 		data = BitMap[ 0 ][ line ] ;
 	
-		// This is where the bit-shifting happens to pull out
+		// this is where the bit-shifting happens to pull out
 		// each LED from a row. If the bit is 1, then the LED
-		// is turned on, otherwise it is turned off.
+		// is turned on, otherwise it is turned off
 		//
 		for ( uint8_t led = 0 ; led < 14 ; ++led ) 
 		{
--- lolshield/nygiants/nygiants.h
+++ lolshield/nygiants/nygiants.h
@@ -7,6 +7,11 @@
 // author: @jefforulez
 //
 
+//
+// based on lol shield library sample code
+// http://code.google.com/p/lolshield/
+//
+
 #ifndef rulez_nygiants_h
 #define rulez_nygiants_h
 
