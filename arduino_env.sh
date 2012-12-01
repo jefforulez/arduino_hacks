868b6f8ffaaa8b515ca9ba36610d34b19698699e
jeff oconnell <jeff.oconnell@gmail.com>
2012-12-01 17:30:47 -0500

    updating environement setup script for newly forked Arduino-Makefile repo

---
+++ arduino_env.sh
@@ -0,0 +1,12 @@
+
+export ARDUINO_DIR="/Applications/Arduino-1.0.2.app/Contents/Resources/Java"
+export ARDMK_DIR="/usr/local"
+
+export ARDUINO_REPO_DIR="/Users/jeffo/local-dropbox/arduino"
+
+export ARDUINO_SKETCHBOOK="$ARDUINO_REPO_DIR/arduino_hacks/libraries"
+
+export ARDUINO_MAKEFILE_DIR="$ARDUINO_REPO_DIR/Arduino-Makefile/arduino-mk"
+export ARDUINO_MAKEFILE="$ARDUINO_MAKEFILE_DIR/Arduino.mk"
+
+export PATH="ARDUINO_MAKEFILE_DIR/bin:$PATH"
